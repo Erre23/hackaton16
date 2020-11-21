338711ddbf73aa651a7dd3368d7f32dc0d7ba04f
Eduardo Rodriguez <lalorodrigez538@gmail.com>
2020-11-21 12:55:03 -0500

    Preguntas

--- src/views/Preguntas.vue
+++ src/views/Preguntas.vue
@@ -5,24 +5,29 @@
             <div class="card-header bg-info text-white">
                 ¿Qué es Redux?
             </div>
-            <div class="card-body">
-                
+            <div class="card-body text-justify">
+                Redux es un patrón de arquitectura de datos que permite manejar el estado de la aplicación de una manera predecible. Está pensado para reducir el número de relaciones entre componentes de la aplicación y mantener un flujo de datos sencillo.
             </div>
         </div>
         <div class="card m-1">
             <div class="card-header bg-info text-white">
                 ¿Qué es Vuex?
             </div>
-            <div class="card-body">
-                
+            <div class="card-body text-justify">
+                Básicamente es un almacén de datos centralizado para usar dentro de una aplicación.
             </div>
         </div>
         <div class="card m-1">
             <div class="card-header bg-info text-white">
-                ¿QPor qué usar un administrador de estado?
+                ¿Por qué usar un administrador de estado?
             </div>
-            <div class="card-body">
-                
+            <div class="card-body text-justify">
+                Podriamos listar los siguientes beneficios.
+                <ul>
+                    <li>Arquitectura escalable de datos</li>
+                    <li>Mayor control sobre el flujo de datos y el estado de la aplicación</li>
+                    <li>Estado global e inmutable</li>
+                </ul>
             </div>
         </div>
     </div>
